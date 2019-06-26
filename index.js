7a81c6256c1f090369bd8b4654618a478e847cc3
Aidanchase <aidanmro@gmail.com>
2019-06-26 19:45:57 +0000

    summed the numbers.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+function sum(a, b) {
+if (a === b){
+  return b;
+}
+return a + sum(a + 1, b)
 }
 
+console.log(sum(3, 7))
 module.exports = sum;
